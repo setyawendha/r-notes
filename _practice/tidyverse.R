c1d95fda4e813e6ee3a5aff6b6cc586afc4d02f5
Homer White <homerhanumat@gmail.com>
2017-12-18 09:10:06 -0500

    revise files chapter

--- _practice/tidyverse.R
+++ _practice/tidyverse.R
@@ -1,21 +1,21 @@
-library(tidyverse)
-data("m111survey", package = "tigerstats")
-m111survey %>% 
-  filter(sex == "male") %>% 
-  select(height, extra_life)
-data("babynames", package = "babynames")
-babynames %>% 
-  filter(name == "Prince" & year >= 1978)
-class(m111survey)
-class(babynames)
-m111survey <-
-  as_tibble(m111survey)
-m111survey
-
-"Hello" %>% cat()
-"Hello" %>% rep(times = 4)
-1:20 %>% sum()
-letters %>% .[4]
-4 %>% rep("hello", times = .)
-rep(letters, times = 1:length(letters))
-letters %>% rep(times = 1:length(.))
+#library(dplyr)
+#library(stringr)
+library(magrittr)
+c("aa", "ba", "b") %>% 
+  str_subset(., "a") == "ba" %>% 
+  length()
+c("aa", "ba", "b") %>% 
+  `==`(str_subset(., "a"), "ba") %>% 
+  length()
+`==`(1:3, 2)
+`==`(str_subset(c("aa", "ba", "b"), "a"), "ba")
+LETTERS[1:3] %>% tolower() == "b"
+LETTERS[1:3] %>% tolower() == "b" %>% length()
+1:3 %>% sqrt() + 2 %>% length()
+1:3 %>% sqrt() + add(2) %>% length()
+LETTERS[1:3] %>% equals(tolower(), "b") %>% length()
+1:10 %>%  *2 %>% sum
+1:10 %>% .*2 %>% sum
+(1:3 %>% sqrt() + 3) %>% length()
+library(stringr)
+(c("aa", "ba", "b") %>% str_count("a") == 2) %>% sum()
