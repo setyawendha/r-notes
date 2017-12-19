a2fc04cd4a4b78b00e92ecd654f655bc2e2e5c23
Homer White <homerhanumat@gmail.com>
2017-12-19 11:26:14 -0500

    revise functional and oo chapters; starting to lose references to R-packages

--- _practice/tidyverse.R
+++ _practice/tidyverse.R
@@ -1,21 +1,15 @@
 #library(dplyr)
-#library(stringr)
-library(magrittr)
-c("aa", "ba", "b") %>% 
-  str_subset(., "a") == "ba" %>% 
-  length()
-c("aa", "ba", "b") %>% 
-  `==`(str_subset(., "a"), "ba") %>% 
-  length()
-`==`(1:3, 2)
-`==`(str_subset(c("aa", "ba", "b"), "a"), "ba")
-LETTERS[1:3] %>% tolower() == "b"
-LETTERS[1:3] %>% tolower() == "b" %>% length()
-1:3 %>% sqrt() + 2 %>% length()
-1:3 %>% sqrt() + add(2) %>% length()
-LETTERS[1:3] %>% equals(tolower(), "b") %>% length()
-1:10 %>%  *2 %>% sum
-1:10 %>% .*2 %>% sum
-(1:3 %>% sqrt() + 3) %>% length()
-library(stringr)
-(c("aa", "ba", "b") %>% str_count("a") == 2) %>% sum()
+upperBounds <- c(5, 7, 8, 2, 9)
+upperBounds %>%
+  map(function(x) {
+    runif(5, max = x)
+  })
+
+upperBounds %>% map(~runif(n = 5, max = .))
+lowerBounds <- upperBounds - 3
+n <- 1:5
+pmap(list(lowerBounds, upperBounds), runif, n = 5)
+pmap(list(n, lowerBounds, upperBounds), runif)
+pmap(list(n, lowerBounds), runif, max = 10)
+
+upperBounds <- c(5, 7, 8, 2, 9)
